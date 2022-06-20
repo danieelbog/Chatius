08ef462f26ac7fd9a1e219b2c8a2afab8e5354b9
Daniel Bogdan <d.bogdan@interworks.cloud>
2022-06-20 14:41:35 +0300

    - remove bootstrap and bootstrap-vue

--- WebApp.SPA/ClientApp/src/app/main.ts
+++ WebApp.SPA/ClientApp/src/app/main.ts
@@ -1,7 +1,6 @@
 import App from "./App.vue";
 import router from "../router";
 import VueAxios from "vue-axios";
-import BootstrapVue3 from "bootstrap-vue-3";
 
 import { createApp } from "vue";
 import { api } from "../app/axios/index";
@@ -17,7 +16,6 @@ const app = createApp(App);
 app.use(VueAxios, api);
 app.use(mockApiAdapter, api);
 app.use(pinia);
-app.use(BootstrapVue3);
 app.use(router);
 
 app.mount("#app");
